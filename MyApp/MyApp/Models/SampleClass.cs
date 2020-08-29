6003463160f85aa42939c999a3a3229a88bd7bcb
kusa-mochi <whoatemyapplepie@gmail.com>
2020-08-29 22:11:49 +0900

    必要なクラスと構造体をつくった。

---
+++ MyApp/MyApp/Models/SampleClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Models
+{
+    public class SampleClass
+    {
+        public int SampleProperty { get; set; } = 0;
+
+        public void InSample(in int n)
+        {
+            //// in修飾子が付けられた引数nの値は変更できないため、コンパイルエラーとなる。
+            // n++;
+        }
+
+        public void OutSample(out int n)
+        {
+            //// 未割り当ての変数nの値が使われるため、コンパイルエラーとなる。
+            // n++;
+
+            // 次のように値を代入する分には問題ない。
+            n = 123;
+        }
+
+        public void RefSample(ref int n)
+        {
+            n++;
+        }
+    }
+}
---
+++ MyApp/MyApp/Structs/SampleStruct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Structs
+{
+    public struct SampleStruct
+    {
+        public int SampleMember;
+    }
+}
